9d394016c40719ccaf6f0d9e7fdbde99d5e60145
iuri dotta <idotta@neurovirtual.com>
2024-07-16 10:37:50 -0300

    Created the working sample project with rpp and vcpkg in manifest mode.

---
+++ rpp-msbuild/rpp-msbuild.cpp
@@ -0,0 +1,13 @@
+#include <rpp/rpp.hpp>
+#include <functional>
+#include <iostream>
+
+int main()
+{
+	rpp::source::from_callable(&::getchar)
+		| rpp::operators::repeat()
+		| rpp::operators::take_while([](char v) { return v != '0'; })
+		| rpp::operators::filter(std::not_fn(&::isdigit))
+		| rpp::operators::map(&::toupper)
+		| rpp::operators::subscribe([](char v) { std::cout << v; });
+}
